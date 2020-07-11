21ca43eee0f97fcfe57e80d00a3e87690a0afb7f
vkazei <vkazei@gmail.com>
2020-07-11 22:10:30 +0300

    debug python api

--- api/python/rsft.py
+++ api/python/rsft.py
@@ -3,10 +3,12 @@ import m8r as sf
 import numpy as np
 import subprocess
 import os
+from matplotlib import pyplot as plt
+import string
 
 def cmd(command, return_output=False):
     """Run command and pipe what you would see in terminal into the output cell.
-    If return_output=True the output is returned as a string.
+    If return_output=True the output is also returned as a string.
     
     Args:
     input: command (string) to be executed in the terminal
@@ -31,15 +33,14 @@ def cmd(command, return_output=False):
 
 def to_np(fname):
     '''
-    return numpy array read from .rsf file
+    returns numpy array read from .rsf file
     
     input: file name
     output: numpy array axis 0 is matching
     '''
     f = sf.Input(fname)
     np_arr = f.read().T
-    
-    assert np_arr.shape[0] == f.get("n1")
+    assert np_arr.shape[0] == int(f.get("n1"))
     return np_arr
 
 def vd(fname):
@@ -48,7 +49,7 @@ def vd(fname):
     
     input: file name
     output : variable dictionary vd, 
-    e.g. vd["n1"] first dimension in rsf file
+    e.g. vd["n1"] contains the first dimension in rsf file
     '''
     header = cmd(f"sfin {fname}", return_output=True)
     print(header)
@@ -60,13 +61,26 @@ def write_vd(fname, vd):
     
     input: file name, variable dictionary
     '''
-    f = sf.Output(fname)
     for key, val in vd.items():
-        f.put(key, val)
-    f.close()
+        cmd(f"sfput < {fname} {key}={val} > .tmp.{fname}")
+        cmd(f"sfmv .tmp.{fname}  {fname}")
     return 0
 
-def write_arr(fname, arr):
+def np_to_rsf(vel, model_output, d1 = 1, d2 = 1):
+    ''' Write 2D numpy array vel to rsf file model_output '''
+    yy = sf.Output(model_output)
+    yy.header = ''
+    yy.put('n1',np.shape(vel)[1])
+    yy.put('n2',np.shape(vel)[0])
+    yy.put('d1',d1)
+    yy.put('d2',d2)
+    yy.put('o1',0)
+    yy.put('o2',0)
+    yy.headerflushed = True
+    yy.write(vel)
+    yy.close()
+
+def write_arr(fname, arr, vd=None):
     '''
     writes array to a file fname
     (?need to check shapes before writing?)
@@ -74,10 +88,33 @@ def write_arr(fname, arr):
     input:  file name, numpy array
     '''
     f = sf.Output(fname)
-    f.write(arr)
+    f.header = ''
+    i = 1
+    for n in arr.shape:
+        print(f"set n{i}={n}")
+        f.put(f"n{i}", n)
+        if vd==None:
+            f.put(f"d{i}",1.0)
+            f.put(f"o{i}",0.0)
+        else:
+            assert vd[f"n{i}"]==n
+        i += 1
+    
+    if vd != None:
+        for key, val in vd.items():
+            print(f"{key}={val}")
+            #if (key[0] in ["n","o","d"]) and (key[1] in string.digits):
+            f.put(key, val)
+    f.write(arr.T)
     f.close()
-    assert 
     return 0
+
+def view2d(fname):
+    plt.imshow(to_np(fname))
+    plt.axis("tight")
+    plt.colorbar()
+    plt.show()
+    
 
 def parse_header_to_dict(header):
     """Parse RSF header into a dictionary of variables, modified from m8r.py
@@ -117,7 +154,11 @@ def parse_header_to_dict(header):
             else:
                 rhs_out = rhs
                 pos += 1
-            vd[lhs] = rhs_out
+            if lhs[0]=="n" and (lhs[1] in string.digits):
+                rhs_out = int(rhs_out)
+            elif (lhs[0] in ["d","o"]) and (lhs[1] in string.digits):
+                rhs_out = float(rhs_out)
+            vd[lhs] = rhs_out   
         else:
             pos += 1
     return vd
@@ -129,8 +170,8 @@ class rsf():
         self.read()
     
     def write(self):
-        write_vd(self.fname, self.vd)
-        write_arr(self.fname, self.arr)
+        #write_vd(self.fname, self.vd)
+        write_arr(self.fname, self.arr, self.vd)
     
     def read(self):
         fname = self.fname
